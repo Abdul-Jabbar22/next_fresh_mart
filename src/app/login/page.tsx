3ac014c209bcb0c840f60987189a089913176067
Abdul-Jabbar22 <abduljabbarrar@example.com>
2025-05-05 20:06:59 +0500

    try to add search

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -20,7 +20,7 @@ const LoginPage = () => {
     setSuccess("");
 
     try {
-      const response = await fetch("http://localhost:3000/api/login", {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000'}/api/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
